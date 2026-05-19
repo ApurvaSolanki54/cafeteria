582e89d4af06a2de446b503bb4d0c0be588aec71
apurva470 <apurvasolanki70@gmail.com>
2026-05-19 23:10:09 +0530

    added /api/**

--- src/main/java/com/example/cafeteria/config/CorsConfig.java
+++ src/main/java/com/example/cafeteria/config/CorsConfig.java
@@ -32,7 +32,7 @@ public class CorsConfig {
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration("/api/**", config);
         return new CorsFilter(source);
     }
 }
